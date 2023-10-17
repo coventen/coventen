84dafca280426678b8eec7a5b2b637d2f20d2cb7
tausifabid12 <tasifabid12@gmail.com>
2023-10-17 14:17:24 +0600

    deploy problem fixed

--- src/app/desktopHome/projects/create/Main.tsx
+++ src/app/desktopHome/projects/create/Main.tsx
@@ -328,12 +328,12 @@ const Main = () => {
 
                                     <div className="col-span-full">
                                         <label htmlFor="address">Project name</label>
-                                        <input type="text" id="address" className="h-10 border border-gray-300 mt-1 rounded px-4 w-full " placeholder=""   {...register('testName')} />
+                                        <input type="text" id="address" className="h-10 border border-gray-300 mt-1 rounded px-4 w-full " placeholder=""   {...register('projectName')} />
                                     </div>
 
                                     <div className="col-span-full">
                                         <label htmlFor="city">Project Description</label>
-                                        <textarea rows={4} id="city" className=" border border-gray-300 mt-1 rounded px-4 w-full " placeholder="" {...register("testDescription")} />
+                                        <textarea rows={4} id="city" className=" border border-gray-300 mt-1 rounded px-4 w-full " placeholder="" {...register("projectDescription")} />
                                     </div>
 
 
